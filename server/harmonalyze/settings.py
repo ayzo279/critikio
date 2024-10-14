5502854aa6c504c22877894489a337a93672f9b5
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-14 18:10:26 -0400

    added hosting permission

--- server/harmonalyze/settings.py
+++ server/harmonalyze/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
