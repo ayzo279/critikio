9ecae3bab831762661c702cbad5974feda0f445e
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-14 20:04:47 -0400

    fixed model to accommodate request data

--- server/recommendations/models.py
+++ server/recommendations/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 class Recommendation(models.Model):
     # user_id = models.CharField(max_length=255)
-    song_name = models.CharField(max_length=255)
-    artist_name = models.CharField(max_length=255)
-    # filters = models.JSONField()
-    recommended_songs = models.JSONField()
+    reference_track = models.CharField(max_length=255)  # For referenceTrack
+    reference_artist = models.CharField(max_length=255)  # For referenceArtist
+    toggles = models.JSONField()  # To store toggles as a dictionary
+    badges = models.JSONField()  # Assuming you're using Django 3.1+ for the JSONField to store lists
+    song_name = models.CharField(max_length=255, null=True)  # For song_name
+    artist_name = models.CharField(max_length=255, null=True)  # For artist_name
+    recommended_songs = models.JSONField(null=True)  # For a list of recommended songs
     # created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
