d364488c5fa469c82fd665c0e1a447b57c2b1eae
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-14 20:12:10 -0400

    fixed incorrect naming in models

--- server/recommendations/models.py
+++ server/recommendations/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Recommendation(models.Model):
     # user_id = models.CharField(max_length=255)
-    reference_track = models.CharField(max_length=255)  # For referenceTrack
-    reference_artist = models.CharField(max_length=255)  # For referenceArtist
+    referenceTrack = models.CharField(max_length=255)  # For referenceTrack
+    referenceArtist = models.CharField(max_length=255)  # For referenceArtist
     toggles = models.JSONField()  # To store toggles as a dictionary
     badges = models.JSONField()  # Assuming you're using Django 3.1+ for the JSONField to store lists
     song_name = models.CharField(max_length=255, null=True)  # For song_name
