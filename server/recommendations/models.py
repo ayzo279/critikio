75dfa83462e408611ed7d3218abb7a2affdfef85
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-14 20:31:08 -0400

    more model changes

--- server/recommendations/models.py
+++ server/recommendations/models.py
@@ -3,13 +3,11 @@ from django.db import models
 
 class Recommendation(models.Model):
     # user_id = models.CharField(max_length=255)
-    referenceTrack = models.CharField(max_length=255)  # For referenceTrack
-    referenceArtist = models.CharField(max_length=255)  # For referenceArtist
-    toggles = models.JSONField()  # To store toggles as a dictionary
-    badges = models.JSONField()  # Assuming you're using Django 3.1+ for the JSONField to store lists
-    song_name = models.CharField(max_length=255, null=True)  # For song_name
-    artist_name = models.CharField(max_length=255, null=True)  # For artist_name
-    recommended_songs = models.JSONField(null=True)  # For a list of recommended songs
+    referenceTrack = models.CharField(max_length=255)
+    referenceArtist = models.CharField(max_length=255)  
+    toggles = models.JSONField(default=dict)
+    badges = models.JSONField(default=dict) 
+    recommended_songs = models.JSONField(null=True)  
     # created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
