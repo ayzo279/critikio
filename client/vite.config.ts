974e932171dcbddfed76bbcfa0745bf7bae74bc2
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-15 01:06:38 -0400

    modified server port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,12 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/recommendation':{
-        target: 'https://harmonalyze.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+    host: '0.0.0.0',
   }
 })
