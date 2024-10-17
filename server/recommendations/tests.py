48743214cf1d22f15fea4dcb290bee34c3bd430f
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-16 20:29:56 -0400

    fixed spotify api calls

--- server/recommendations/tests.py
+++ server/recommendations/tests.py
@@ -55,39 +55,38 @@ class RecommendationsTestCase(TestCase):
         self.assertIsInstance(similarity, float)
         self.assertLessEqual(similarity, 1.0)
 
-    # def test_get_recommendations(self):
-    #     song = "Roxanne"
-    #     artist = "The Police"
-    #     artist_list = ["Bruno Mars"]
-    #     filters = {
-    #         "danceability": True,
-    #         "valence": True
-    #     }
+    def test_get_recommendations(self):
+        song = "Roxanne"
+        artist = "The Police"
+        artist_list = ["Bruno Mars"]
+        filters = {
+            "danceability": True,
+            "valence": True
+        }
 
-    #     recommendations = get_recommendations(song, artist, artist_list, filters, count=3)
-    #     self.assertIsInstance(recommendations, dict)
-    #     self.assertLessEqual(len(recommendations), 3)
+        recommendations = get_recommendations(song, artist, artist_list, filters, count=3)
+        print(recommendations)
+        self.assertIsInstance(recommendations, list)
+        self.assertLessEqual(len(recommendations), 3)
 
-    # def test_process_recommendations_success(self):
-    #     # Mock the return value of get_recommendations
-    #     reference_track = "Shake it off"
-    #     reference_artist = "Taylor Swift"
-    #     filters = {"danceability":True, "energy":True}
-    #     artists = ["Taylor Swift"]
+    def test_process_recommendations_success(self):
+        # Mock the return value of get_recommendations
+        reference_track = "Shake it off"
+        reference_artist = "Taylor Swift"
+        filters = {"danceability":True, "energy":True}
+        artists = ["Taylor Swift"]
 
-    #     recommended_songs = get_recommendations(reference_track, reference_artist, artists, filters)
+        recommended_songs = get_recommendations(reference_track, reference_artist, artists, filters)
 
-    #     print(recommended_songs)
+        print(recommended_songs)
 
     def test_get_global_recommendations(self):
         reference_track = "Shape of You"
         reference_artist = "Ed Sheeran"
         filters = {"danceability":True, "energy":True}
-        get_global_recs(reference_track, reference_artist, filters)
-        # print(recommended_songs)
+        recommended_songs = get_global_recs(reference_track, reference_artist, filters)
+        print(recommended_songs)
 
-    # def test_api(self):
-    #     print(test_api())
 
 
     
--- server/recommendations/views.py
+++ server/recommendations/views.py
@@ -27,8 +27,6 @@ class RecommendationViewSet(viewsets.ModelViewSet):
         artists = request.data.get('badges', [])
 
         rec_list = get_recommendations(referenceTrack, referenceArtist, artists, filters)
-
-        # print(rec_list)
         
         recommendation = Recommendation(
             referenceTrack=referenceTrack,
@@ -80,45 +78,38 @@ def get_recommendations(song, artist, artist_list, filters, count=5):
     input_track_features["loudness"] = np.clip(np.interp(input_track_features["loudness"], [-60, 0], [0, 1]), 0, 1)
     input_track_features["time_signature"] = np.interp(input_track_features["time_signature"], [3, 7], [0, 1])
 
+    all_tracks = []
     for artist_id in artist_ids:
         albums = []
         results = rate_limited_api_call(sp.artist_albums, artist_id=artist_id, album_type='album', limit=15)
         albums.extend(results['items'])
-
-        all_tracks = []
-        if len(albums) < 3:
-            sample_size = 15
 
         for album in albums:
             album_id = album['id']
             tracks = rate_limited_api_call(sp.album_tracks, album_id)['items']
             all_tracks.extend(tracks)
 
-        artist_rankings = {}
+    all_rankings = {}
 
-        batched_tracks = batch_process(all_tracks)
-        for batch in batched_tracks:
-            try:
-                track_features = rate_limited_api_call(sp.audio_features, [track["id"] for track in batch])
-            except spotipy.exceptions.SpotifyException as e:
-                if e.http_status == 429:  # Too Many Requests
-                    print("Rate limit exceeded. Try again later")
-                    return []
-            if track_features is not None:
-                for i in range(len(track_features)):
-                    similarity = compute_similarity(input_track_features, track_features[i], filters)
-                    artist_rankings[batch[i]['id']] = similarity
+    batched_tracks = batch_process(all_tracks)
+    for batch in batched_tracks:
+        try:
+            track_features = rate_limited_api_call(sp.audio_features, [track["id"] for track in batch])
+        except spotipy.exceptions.SpotifyException as e:
+            if e.http_status == 429:  # Too Many Requests
+                print("Rate limit exceeded. Try again later")
+                return []
+        if track_features is not None:
+            for i in range(len(track_features)):
+                similarity = compute_similarity(input_track_features, track_features[i], filters)
+                all_rankings[batch[i]['id']] = similarity
 
-        # Sort & update rankings and retrieve top `count` tracks
-        artist_rankings = sorted(artist_rankings.items(), key=lambda item: item[1], reverse=True)[:count]
+    # Sort & update rankings and retrieve top `count` tracks
+    top_ranks = dict(sorted(all_rankings.items(), key=lambda item: item[1], reverse=True)[:count])
         
-        for track_id, similarity in artist_rankings:
-            rankings[track_id] = similarity
-
-    rankings = sorted(rankings.items(), key=lambda item: item[1], reverse=True)[:count]
-    tracks_info = rate_limited_api_call(sp.tracks, [track_id for track_id in rankings.keys()])
-    for track in tracks_info:
-        recommendations.append([track['id'], track['artists'][0]['name'], rankings[track['id']], track['album']['images'][0]['url']])
+    tracks_info = rate_limited_api_call(sp.tracks, [track_id for track_id in top_ranks.keys()])
+    for track in tracks_info['tracks']:
+        recommendations.append([track['name'], track['artists'][0]['name'], top_ranks[track['id']], track['album']['images'][0]['url']])
 
     return recommendations
 
@@ -129,6 +120,7 @@ def get_global_recs(song, artist, filters, count=5):
     
     hot100_id = "6UeSakyzhiEt4NB3UAd6NQ"
     global_tracks = rate_limited_api_call(sp.playlist_tracks, hot100_id)
+    global_track_ids = [track['track']['id'] for track in global_tracks['items']]
     search_query = f"track:{song} artist:{artist}"
 
     result = rate_limited_api_call(sp.search, q=search_query, type='track', limit=1)
@@ -138,7 +130,6 @@ def get_global_recs(song, artist, filters, count=5):
         return []  # Return an empty dict if song not found
 
     track_id = result['tracks']['items'][0]['id']
-    # print(sp.audio_features(track_id))
     try:
         input_track_features = rate_limited_api_call(sp.audio_features, track_id)[0]
     except spotipy.exceptions.SpotifyException as e:
@@ -146,41 +137,33 @@ def get_global_recs(song, artist, filters, count=5):
             print("Rate limit exceeded. Try again later")
             return []
 
-    print(input_track_features)
     # # Normalize features
-    # input_track_features["tempo"] = np.clip(np.interp(input_track_features["tempo"], [0, 200], [0, 1]), 0, 1)
-    # input_track_features["key"] = np.interp(input_track_features["key"], [-1, 11], [0, 1])
-    # input_track_features["loudness"] = np.clip(np.interp(input_track_features["loudness"], [-60, 0], [0, 1]), 0, 1)
-    # input_track_features["time_signature"] = np.interp(input_track_features["time_signature"], [3, 7], [0, 1])
+    input_track_features["tempo"] = np.clip(np.interp(input_track_features["tempo"], [0, 200], [0, 1]), 0, 1)
+    input_track_features["key"] = np.interp(input_track_features["key"], [-1, 11], [0, 1])
+    input_track_features["loudness"] = np.clip(np.interp(input_track_features["loudness"], [-60, 0], [0, 1]), 0, 1)
+    input_track_features["time_signature"] = np.interp(input_track_features["time_signature"], [3, 7], [0, 1])
 
-    # try:
-    #     global_features = rate_limited_api_call(sp.audio_features, global_tracks)
-    # except spotipy.exceptions.SpotifyException as e:
-    #     if e.http_status == 429:  # Too Many Requests
-    #         print("Rate limit exceeded. Try again later")
-    #         return []
+    try:
+        global_features = rate_limited_api_call(sp.audio_features, global_track_ids)
+    except spotipy.exceptions.SpotifyException as e:
+        if e.http_status == 429:  # Too Many Requests
+            print("Rate limit exceeded. Try again later")
+            return []
     
-    # rankings = {}
-    # recommendations = []
+    rankings = {}
+    recommendations = []
     
-    # for track in global_features:
-    #     rankings[track['id']] = compute_similarity(input_track_features, track, filters)
+    for track in global_features:
+        rankings[track['id']] = compute_similarity(input_track_features, track, filters)
 
-    # rankings = sorted(rankings.items(), key=lambda item: item[1], reverse=True)[:count]
-    # tracks_info = rate_limited_api_call(sp.tracks, [track_id for track_id in rankings.keys()])
-    # for track in tracks_info:
-    #     recommendations.append([track['id'], track['artists'][0]['name'], rankings[track['id']], track['album']['images'][0]['url']])
+    rankings = dict(sorted(rankings.items(), key=lambda item: item[1], reverse=True)[:count])
+    track_list = [track_id for track_id in rankings.keys()]
+    tracks_info = rate_limited_api_call(sp.tracks, track_list)
+    for track in tracks_info['tracks']:
+        recommendations.append([track['name'], track['artists'][0]['name'], rankings[track['id']], track['album']['images'][0]['url']])
 
-    # return recommendations
+    return recommendations
 
-    
-def test_api():
-    client_id = os.getenv("SPOTIFY_CLIENT_ID")
-    client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
-    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
-
-    track = sp.playlist_tracks("6UeSakyzhiEt4NB3UAd6NQ")
-    print(track)
 
 
 
