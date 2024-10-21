83d80996a3950f95312880c35b6f8dc0ee8a121c
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-20 22:24:49 -0400

    url routing issues fixed

--- server/harmonalyze/urls.py
+++ server/harmonalyze/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recommendations.views import RecommendationViewSet
+from clustering.views import ClusteringViewSet
 
+# Create routers for each app
+recommendation_router = DefaultRouter()
+recommendation_router.register(r'recommendations', RecommendationViewSet)
+
+clustering_router = DefaultRouter()
+clustering_router.register(r'clustering', ClusteringViewSet)
+
+# Main URL patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recommendations.urls')),
-    path('', include('clustering.urls')),
+    path('', include(recommendation_router.urls)),  
+    path('', include(clustering_router.urls)), 
 ]
