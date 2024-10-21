28dc921bfa3555ad5edc91b0944a3c35e3ac2555
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-20 20:58:29 -0400

    added missing url path to main app

--- server/harmonalyze/urls.py
+++ server/harmonalyze/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recommendations.urls')),
+    path('', include('clustering.urls')),
 ]
