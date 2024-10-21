4855cc52ec741445c0f65a0bc730812dbeb1ca15
Andy Zhuo <azhuo@college.harvard.edu>
2024-10-21 14:52:21 -0400

    ping server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import SigninPage from "./pages/SigninPage";
 import HomePage from "./pages/HomePage";
 import { getUserToken } from "./services/auth";
+import axios from "axios";
+
+const usePingServer = () => {
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      try {
+        await axios.get("https://harmonalyze.onrender.com/recommendation/");
+      } catch (error: any) {
+        console.error(`Error in pinging server: ${error.message}`);
+      }
+    }, 600000); // 10 minutes
+
+    return () => clearInterval(intervalId); // Cleanup on unmount
+  }, []);
+};
 
 const App: React.FC = () => {
+  usePingServer();
+
   return (
     <Router>
       <AuthCheck />
